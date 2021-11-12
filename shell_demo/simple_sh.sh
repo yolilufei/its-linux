eea7dd1e2c34e35ac3b274e5e1f5c31e6df12e70
李录飞 <lilufei03@kuaishou.com>
2021-11-12 14:18:01 +0800

    shell practise

---
+++ shell_demo/simple_sh.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# author: guoshi
+helloshell(){
+    echo "hello shell, this is my first shell function"
+}
+
+echo "----------函数开始执行-----------"
+helloshell
+echo "----------函数执行结束-----------"
